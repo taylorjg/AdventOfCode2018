3367960461a3b12c8f67b3e443720d1a6fb0b619
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-12-01 14:31:44 +0000

    Day 1 part 2 using trampoline and immutable OrderedSet

--- Day01/index.js
+++ Day01/index.js
@@ -1,8 +1,9 @@
 const { promisify } = require('util')
 const fs = require('fs')
-const readFile = promisify(fs.readFile)
 const R = require('ramda')
-const bilby  =require('bilby')
+const bilby = require('bilby')
+const { OrderedSet } = require('immutable')
+const readFile = promisify(fs.readFile)
 
 const part1 = frequencies => {
   const answer = R.sum(frequencies)
@@ -10,20 +11,20 @@ const part1 = frequencies => {
 }
 
 const part2 = frequencies => {
-  const loop = (index, subtotal, subtotals) => {
-    const f = frequencies[index % frequencies.length]
-    const newSubtotal = subtotal + f
-    if (subtotals.has(newSubtotal)) return bilby.done(newSubtotal)
-    subtotals.add(newSubtotal)
-    return bilby.cont(() => loop(index + 1, newSubtotal, subtotals))
+  const loop = subtotals => {
+    const f = frequencies[subtotals.size % frequencies.length]
+    const prevSubtotal = subtotals.isEmpty() ? 0 : subtotals.last()
+    const nextSubtotal = prevSubtotal + f
+    if (subtotals.has(nextSubtotal)) return bilby.done(nextSubtotal)
+    return bilby.cont(() => loop(subtotals.add(nextSubtotal)))
   }
-  const answer = bilby.trampoline(loop(0, 0, new Set()))
+  const answer = bilby.trampoline(loop(OrderedSet()))
   console.log(`part 2 answer: ${answer}`)
 }
 
 const main = async () => {
   const buffer = await readFile("Day01/input.txt")
-  const lines = buffer.toString().split('\n').filter(s => s.length > 0)
+  const lines = buffer.toString().split('\n').filter(R.length)
   const frequencies = lines.map(s => parseInt(s, 10))
   part1(frequencies)
   part2(frequencies)
