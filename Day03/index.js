a73cef424ab34bfe8ab874b8ecedd90e9133dcff
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-12-03 14:10:52 +0000

    Tweaks to day 3

--- Day03/index.js
+++ Day03/index.js
@@ -21,24 +21,24 @@ const parseLines = lines =>
 const makeKey = (x, y) =>
   `${x}-${y}`
 
-const visitSquares = (map, claim, fn, init) => {
+const claimToSquaresList = claim => {
   const xs = R.range(claim.x, claim.x + claim.w)
   const ys = R.range(claim.y, claim.y + claim.h)
-  const squares = R.chain(x => R.map(y => [x, y], ys), xs)
-  const op = (acc, [x, y]) => {
-    const key = makeKey(x, y)
-    const ids = map.get(key) || []
-    return fn(key, ids, acc)
-  }
-  return R.reduce(op, init, squares)
+  return R.chain(x => R.map(y => [x, y], ys), xs)
 }
 
 const makeMap = claims => {
-  const map = new Map()
-  const addSquaresToMap = claim =>
-    visitSquares(map, claim, (key, ids) => map.set(key, [...ids, claim.id]))
-  claims.forEach(addSquaresToMap)
-  return map
+  const claimToSquaresData = claim =>
+    claimToSquaresList(claim).map(([x, y]) => ({
+      key: makeKey(x, y),
+      id: claim.id
+    }))
+  const pipe = R.pipe(
+    R.chain(claimToSquaresData),
+    R.groupBy(e => e.key),
+    R.toPairs,
+    pairs => new Map(pairs))
+  return pipe(claims)
 }
 
 const part1 = claims => {
@@ -51,10 +51,11 @@ const part1 = claims => {
 
 const part2 = claims => {
   const map = makeMap(claims)
-  const countOverlaps = claim => {
-    const fn = (_, ids, acc) => acc + (ids.length > 1 ? 1 : 0)
-    return visitSquares(map, claim, fn, 0)
-  }
+  const countOverlaps = claim => R.reduce(
+    (acc, [x, y]) =>
+      map.get(makeKey(x, y)).length > 1 ? acc + 1 : acc,
+    0,
+    claimToSquaresList(claim))
   const overlapCounts = claims.map(claim => ({
     id: claim.id,
     overlaps: countOverlaps(claim)
@@ -64,8 +65,8 @@ const part2 = claims => {
 }
 
 const main = async () => {
-  const buffer = await readFile("Day03/input.txt")
-  const lines = buffer.toString().split('\n').filter(R.length)
+  const buffer = await readFile('Day03/input.txt', 'utf8')
+  const lines = buffer.split('\n').filter(R.length)
   const claims = parseLines(lines)
   part1(claims)
   part2(claims)
