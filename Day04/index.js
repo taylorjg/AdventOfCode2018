9b9e7ac38e89b6228553ca33cd772bc5e4d80659
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-12-04 19:17:04 +0000

    Day 4 refactorings

--- Day04/index.js
+++ Day04/index.js
@@ -1,3 +1,4 @@
+const I = require('immutable')
 const R = require('ramda')
 const util = require('util')
 const fs = require('fs')
@@ -36,8 +37,8 @@ const guardGuardWakesUp = m => ({
 
 const regexTimestamp = /\[1518-([\d]{2})-([\d]{2}) ([\d]{2}):([\d]{2})\]/
 const regex1 = new RegExp(regexTimestamp.source + / Guard #(\d+) begins shift/.source)
-const regex2 = new RegExp(regexTimestamp.source + / wakes up/.source)
-const regex3 = new RegExp(regexTimestamp.source + / falls asleep/.source)
+const regex2 = new RegExp(regexTimestamp.source + / falls asleep/.source)
+const regex3 = new RegExp(regexTimestamp.source + / wakes up/.source)
 
 const PARSE_TABLE = [
   [regex1, guardBeginsShift],
@@ -56,15 +57,17 @@ const parseLine = line => {
 const parseLines = lines =>
   lines.map(parseLine)
 
-// TODO: make this pure functional!
 const addIds = events => {
-  let currentId
-  events.forEach(e => {
-    if (e.eventType === GUARD_BEGINS_SHIFT)
-      currentId = e.id
-    else
-      e.id = currentId
-  })
+  const reducer = (acc, e) =>
+    e.eventType === GUARD_BEGINS_SHIFT
+      ? { ...acc, currentId: e.id }
+      : { ...acc, newEvents: acc.newEvents.push({ ...e, id: acc.currentId }) }
+  const init = {
+    currentId: undefined,
+    newEvents: I.List()
+  }
+  const { newEvents } = R.reduce(reducer, init, events)
+  return newEvents.toArray()
 }
 
 const totalSleepTime = events => {
@@ -79,16 +82,21 @@ const groupMinutes = events => {
   return R.groupBy(R.identity, minutes)
 }
 
+const timestampAsc = (a, b) => a.timestamp - b.timestamp
+const listLengthDesc = (xs1, xs2) => xs2.length - xs1.length
+const kvpValueNumberDesc = ([, v1], [, v2]) => v2 - v1
+const kvpValueListLengthDesc = ([, vs1], [, vs2]) => listLengthDesc(vs1, vs2)
+
 const part1 = groupedEvents => {
   const totals = R.map(totalSleepTime, groupedEvents)
   const kvps = R.toPairs(totals)
-  const kvpsSorted = R.sort(([, total1], [, total2]) => total2 - total1, kvps)
+  const kvpsSorted = R.sort(kvpValueNumberDesc, kvps)
   const [bestKey] = R.head(kvpsSorted)
   const bestEvents = groupedEvents[bestKey]
   const groupedMinutes = groupMinutes(bestEvents)
   const listsOfSameMinutes = R.values(groupedMinutes)
-  const listsOfSameMinutesSorted = R.sort((a, b) => b.length - a.length, listsOfSameMinutes)
-  const [bestMinute] = R.head(listsOfSameMinutesSorted)
+  const listsOfSameMinutesSortedByDescLength = R.sort(listLengthDesc, listsOfSameMinutes)
+  const [bestMinute] = R.head(listsOfSameMinutesSortedByDescLength)
   const bestId = Number(bestKey)
   const answer = bestId * bestMinute
   console.log(`part 1 answer: ${answer}`)
@@ -97,10 +105,10 @@ const part1 = groupedEvents => {
 const part2 = groupedEvents => {
   const idsToGroupedMinutes = R.map(groupMinutes, groupedEvents)
   const idsToListsOfMinutes = R.map(R.values, idsToGroupedMinutes)
-  const idsToListsOfMinutesSortedByDescLength = R.map(R.sort((a, b) => b.length - a.length), idsToListsOfMinutes)
-  const idsToLongestListOfMinutes = R.map(R.head, idsToListsOfMinutesSortedByDescLength)
-  const kvps = R.toPairs(idsToLongestListOfMinutes)
-  const kvpsSortedByDescLength = R.sort(([, minutes1], [, minutes2]) => minutes2.length - minutes1.length, kvps)
+  const idsToListsOfMinutesSortedByDescLength = R.map(R.sort(listLengthDesc), idsToListsOfMinutes)
+  const idsToBestListOfMinutes = R.map(R.head, idsToListsOfMinutesSortedByDescLength)
+  const kvps = R.toPairs(idsToBestListOfMinutes)
+  const kvpsSortedByDescLength = R.sort(kvpValueListLengthDesc, kvps)
   const [id, minutes] = R.head(kvpsSortedByDescLength)
   const minute = R.head(minutes)
   const answer = id * minute
@@ -111,9 +119,9 @@ const main = async () => {
   const buffer = await readFile('Day04/input.txt', 'utf8')
   const lines = buffer.split('\n').filter(R.length)
   const rawEvents = parseLines(lines)
-  const sortedEvents = rawEvents.sort((a, b) => a.timestamp - b.timestamp)
-  addIds(sortedEvents)
-  const groupedEvents = R.groupBy(e => e.id, sortedEvents.filter(e => e.eventType != GUARD_BEGINS_SHIFT))
+  const sortedEvents = rawEvents.sort(timestampAsc)
+  const sortedEventsWithIds = addIds(sortedEvents)
+  const groupedEvents = R.groupBy(e => e.id, sortedEventsWithIds)
   part1(groupedEvents)
   part2(groupedEvents)
 }
