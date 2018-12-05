964a5d4e5b5bb5b0146a769de84a36b2727222c2
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-12-05 08:06:43 +0000

    Day 5 part 2

--- Day05/index.js
+++ Day05/index.js
@@ -26,11 +26,25 @@ const part1 = polymer => {
   console.log(`part 1 answer: ${answer}`)
 }
 
+const part2 = polymer => {
+  const removeUnitAndReact = unit => {
+    const polymerWithUnitRemoved = polymer.replace(new RegExp(unit, 'ig'), '')
+    return react(polymerWithUnitRemoved).length
+  }
+  const a = "a".codePointAt(0)
+  const z = "z".codePointAt(0) + 1
+  const letters = R.range(a, z).map(n => String.fromCodePoint(n))
+  const lengths = R.map(removeUnitAndReact, letters)
+  const lengthsSortedAsc = R.sort((a, b) => a - b, lengths)
+  const answer = R.head(lengthsSortedAsc)
+  console.log(`part 2 answer: ${answer}`)
+}
+
 const main = async () => {
   const buffer = await readFile('Day05/input.txt', 'utf8')
-  // const buffer = await readFile('Day05/test.txt', 'utf8')
   const polymer = buffer.split('\n')[0]
   part1(polymer)
+  part2(polymer)
 }
 
 main()
