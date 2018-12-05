fed7b1599b4ec541adaa6c5de739a125ccfbfbf4
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-12-05 12:55:51 +0000

    Day 5 - more refactoring

--- Day05/index.js
+++ Day05/index.js
@@ -6,35 +6,45 @@ const fs = require('fs')
 const readFile = util.promisify(fs.readFile)
 
 const findReactingUnits = p => {
-  const reducer = (_, idx) => {
-    const a = p.codePointAt(idx)
-    const b = p.codePointAt(idx + 1)
-    return Math.abs(a - b) === 32 ? idx : -1
+  for (const idx of R.range(0, p.length - 1)) {
+    const codePoint1 = p.codePointAt(idx)
+    const codePoint2 = p.codePointAt(idx + 1)
+    if (Math.abs(codePoint1 - codePoint2) === 32) return idx
   }
-  return R.reduceWhile(pos => pos < 0, reducer, -1, R.range(0, p.length - 1))
+  return -1
 }
 
-const react = p => {
-  const pos = findReactingUnits(p)
-  return pos >= 0
-    ? B.cont(() => react(R.take(pos, p) + R.drop(pos + 2, p)))
-    : B.done(p)
+const destroyUnits = (p, pos) =>
+  R.take(pos, p) + R.drop(pos + 2, p)
+
+const react = polymer => {
+  const loop = p => {
+    const pos = findReactingUnits(p)
+    return pos >= 0
+      ? B.cont(() => loop(destroyUnits(p, pos)))
+      : B.done(p)
+  }
+  return B.trampoline(loop(polymer))
 }
 
 const part1 = polymer => {
-  const answer = B.trampoline(react(polymer)).length
+  const answer = react(polymer).length
   console.log(`part 1 answer: ${answer}`)
 }
 
+const letterRange = (ch1, ch2) => {
+  const codePoint1Incl = ch1.codePointAt(0)
+  const codePoint2Excl = ch2.codePointAt(0) + 1
+  return String.fromCodePoint(...R.range(codePoint1Incl, codePoint2Excl))
+}
+
 const part2 = polymer => {
-  const removeUnitAndReact = unit => {
-    const polymerWithUnitRemoved = polymer.replace(new RegExp(unit, 'ig'), '')
-    return B.trampoline(react(polymerWithUnitRemoved)).length
+  const removeLetterAndReact = letter => {
+    const r = new RegExp(letter, 'ig')
+    const newPolymer = polymer.replace(r, '')
+    return react(newPolymer).length
   }
-  const a = "a".codePointAt(0)
-  const z = "z".codePointAt(0) + 1
-  const letters = R.range(a, z).map(n => String.fromCodePoint(n))
-  const lengths = R.map(removeUnitAndReact, letters)
+  const lengths = R.map(removeLetterAndReact, letterRange("a", "z"))
   const lengthsSortedAsc = R.sort((a, b) => a - b, lengths)
   const answer = R.head(lengthsSortedAsc)
   console.log(`part 2 answer: ${answer}`)
@@ -42,7 +52,7 @@ const part2 = polymer => {
 
 const main = async () => {
   const buffer = await readFile('Day05/input.txt', 'utf8')
-  const polymer = buffer.split('\n')[0]
+  const polymer = buffer.trim()
   part1(polymer)
   part2(polymer)
 }
