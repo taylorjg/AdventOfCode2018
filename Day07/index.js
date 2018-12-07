b3b68e994aa945806f38694e026858c9ad802648
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-12-07 08:18:42 +0000

    Day 7 part 1 - not quite working yet

--- Day07/index.js
+++ Day07/index.js
@@ -16,9 +16,49 @@ const parseLine = line => {
 const parseLines = lines =>
   lines.map(parseLine)
 
+const makeMesh = steps => {
+  let mesh = []
+  const findId = id => mesh.find(e => e.id === id)
+  for (step of steps) {
+    const entry1 = findId(step.id1)
+    if (!entry1) {
+      mesh.push({ id: step.id1, deps: [] })
+    }
+    const entry2 = findId(step.id2)
+    if (entry2) {
+      entry2.deps.push(step.id1)
+    } else {
+      mesh.push({ id: step.id2, deps: [step.id1] })
+    }
+  }
+  return mesh
+}
+
+const orderMesh = mesh => {
+  const loop = (acc, completed) => {
+    const justCompleted = e => {
+      const alreadyCompleted = completed.has(e.id)
+      const allDepsCompleted = e.deps.every(dep => completed.has(dep))
+      return !alreadyCompleted && allDepsCompleted
+    }
+    const v1 = mesh.filter(justCompleted)
+    if (v1.length === 0) return acc
+    const v2 = v1.map(e => e.id)
+    const v3 = v2.sort()
+    const acc2 = acc + v3.join('')
+
+    // Mutable!
+    v3.forEach(id => completed.add(id))
+
+    return loop(acc2, completed)
+  }
+  return loop('', new Set())
+}
+
 const part1 = steps => {
-  steps.forEach(step => console.log(`${step.id1} before ${step.id2}`))
-  const answer = '?'
+  const mesh = makeMesh(steps)
+  console.dir(mesh)
+  const answer = orderMesh(mesh)
   console.log(`part 1 answer: ${answer}`)
 }
 const main = async () => {
