1b43d2cd54c6a7351bb39f673febb5b29792e862
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-12-09 09:41:15 +0000

    Day 9 part 1 - not working yet

---
+++ Day09/index.js
@@ -0,0 +1,36 @@
+const R = require('ramda')
+
+const part1 = (numPlayers, lastMarble) => {
+  const scores = new Map()
+  let currentMarbleIndex = 0
+  let marbles = [0]
+  const turns = R.range(0, lastMarble)
+  // console.log(`elf: [-]; marbles: ${marbles}`)
+  turns.forEach(turn => {
+    const elf = 1 + turn % numPlayers
+    const length = marbles.length
+    const currentMarble = turn + 1
+    if (currentMarble % 23 == 0) {
+      const currentScore = scores.has(elf) ? scores.get(elf) : 0
+      const seventhCcwIndex = (length - (7 - currentMarbleIndex)) % length
+      const seventhCcw = marbles[seventhCcwIndex]
+      scores.set(elf, currentScore + 23 + seventhCcw)
+      marbles = R.remove(seventhCcwIndex, 1, marbles)
+      currentMarbleIndex = seventhCcwIndex % (length - 1)
+    } else {
+      currentMarbleIndex = currentMarbleIndex === length - 1 ? 1 : currentMarbleIndex + 2
+      marbles = R.insert(currentMarbleIndex, currentMarble, marbles)
+    }
+    // console.log(`elf: [${elf}]; currentMarble: ${currentMarble}; currentMarbleIndex: ${currentMarbleIndex}; marbles: ${marbles}`)
+  })
+  const answer = Math.max(...scores.values())
+  console.log(`part 1 answer: ${answer}`)
+}
+
+part1(9, 25)
+part1(10, 1618)
+// part1(13, 7999)
+// part1(17, 1104)
+// part1(21, 6111)
+// part1(30, 5807)
+// part1(410, 72059)
