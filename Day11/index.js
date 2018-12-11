fcdb3ea02e6be0dd79f46bd177a9892f37c9c830
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2018-12-11 10:08:10 +0000

    Day 11 part 2

--- Day11/index.js
+++ Day11/index.js
@@ -11,27 +11,72 @@ const calculatePowerLevelOfFuelCell = serialNumber => ({ x, y }) => {
   return extractHundereds(((rackId * y) + serialNumber) * rackId) - 5
 }
 
-const calculatePowerLevelOfRegion = (serialNumber, { x, y }) => {
-  const xs = R.range(x, x + 3)
-  const ys = R.range(y, y + 3)
-  const fuelCells = R.chain(x => R.map(y => ({ x, y }), ys), xs)
-  return R.sum(fuelCells.map(calculatePowerLevelOfFuelCell(serialNumber)))
+const calculatePowerLevelOfRegion = (serialNumber, size, previousMap, region) => {
+  const key = makeKey(region)
+  const { x, y } = region
+  if (previousMap.has(key)) {
+    const maxX = x + size - 1
+    const maxY = y + size - 1
+    const xysLastCol = R.range(y, y + size - 1).map(y => ({ x: maxX, y }))
+    const xysLastRow = R.range(x, x + size).map(x => ({ x, y: maxY }))
+    const sumLastCol = R.sum(xysLastCol.map(calculatePowerLevelOfFuelCell(serialNumber)))
+    const sumLastRow = R.sum(xysLastRow.map(calculatePowerLevelOfFuelCell(serialNumber)))
+    return previousMap.get(key) + sumLastCol + sumLastRow
+  } else {
+    const xs = R.range(x, x + size)
+    const ys = R.range(y, y + size)
+    const fuelCells = R.chain(x => R.map(y => ({ x, y }), ys), xs)
+    return R.sum(fuelCells.map(calculatePowerLevelOfFuelCell(serialNumber)))
+  }
+}
+
+const calculatePowerLevelsOfRegionsWithSize = (serialNumber, size, previousMap) => {
+  const max = 300 - size + 2
+  const xs = R.range(1, max)
+  const ys = R.range(1, max)
+  const regions = R.chain(x => R.map(y => ({ x, y }), ys), xs)
+  return regions.map(region => ({
+    region,
+    size,
+    powerLevel: calculatePowerLevelOfRegion(serialNumber, size, previousMap, region)
+  }))
+}
+
+const makeKey = region => `${region.x}-${region.y}`
+
+const bestRegionWithSize = (serialNumber, size, previousMap) => {
+  console.log(`[bestRegionWithSize] serialNumber: ${serialNumber}; size: ${size}`)
+  const powerLevels = calculatePowerLevelsOfRegionsWithSize(serialNumber, size, previousMap)
+  const map = new Map(powerLevels.map(pl => [makeKey(pl.region), pl.powerLevel]))
+  const sorted = powerLevels.sort((a, b) => b.powerLevel - a.powerLevel)
+  return [R.head(sorted), map]
 }
 
 const part1 = serialNumber => {
-  const xs = R.range(0, 299)
-  const ys = R.range(0, 299)
-  const regions = R.chain(x => R.map(y => ({ x, y }), ys), xs)
-  const powerLevels = regions.map(region => ({
-    region,
-    powerLevel: calculatePowerLevelOfRegion(serialNumber, region)
-  }))
-  const sorted = powerLevels.sort((a, b) => b.powerLevel - a.powerLevel)
-  const bestRegion = R.head(sorted).region
-  const answer = `${bestRegion.x},${bestRegion.y}`
+  const [best] = bestRegionWithSize(serialNumber, 3, new Map())
+  const answer = `${best.region.x},${best.region.y}`
   console.log(`part 1 answer: ${answer}`)
 }
 
-part1(18)
-part1(42)
+const part2 = serialNumber => {
+  const sizes = R.range(1, 301)
+  const reducer = (acc, size) => {
+    const [best, previousMap] = bestRegionWithSize(serialNumber, size, acc.previousMap)
+    return {
+      bests: [...acc.bests, best],
+      previousMap
+    }
+  }
+  const init = {
+    bests: [],
+    previousMap: new Map()
+  }
+  const { bests } = sizes.reduce(reducer, init)
+  const sorted = bests.sort((a, b) => b.powerLevel - a.powerLevel)
+  const best = R.head(sorted)
+  const answer = `${best.region.x},${best.region.y},${best.size}`
+  console.log(`part 2 answer: ${answer}`)
+}
+
 part1(7139)
+part2(7139)
